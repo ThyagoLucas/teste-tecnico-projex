e06a5733ace962da24f89c2bcda22a2415bfca4e
Thyago Lucas <thyago.devin@gmail.com>
2023-03-29 00:22:24 -0400

    feat: downgrade angular version to use @nebular/theme

--- frontend/projex/src/app/app.module.ts
+++ frontend/projex/src/app/app.module.ts
@@ -3,24 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './src/app/pages/home/home.component';
-import { HomePageComponent } from './pages/home-page/home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { LoginComponent } from './features/login/login.component';
-import { PropertyComponent } from './features/property/property.component';
-import { RegisterComponent } from './features/register/register.component';
-import { StorageComponent } from './shareds/storage/storage.component';
+import { SignInComponent } from './features/auth/components/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HomePageComponent,
-    LoginPageComponent,
-    LoginComponent,
-    PropertyComponent,
-    RegisterComponent,
-    StorageComponent
+    SignInComponent
   ],
   imports: [
     BrowserModule,
--- frontend/projex/src/app/features/auth/components/sign-in/sign-in.component.spec.ts
+++ frontend/projex/src/app/features/auth/components/sign-in/sign-in.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PropertyComponent } from './property.component';
+import { SignInComponent } from './sign-in.component';
 
-describe('PropertyComponent', () => {
-  let component: PropertyComponent;
-  let fixture: ComponentFixture<PropertyComponent>;
+describe('SignInComponent', () => {
+  let component: SignInComponent;
+  let fixture: ComponentFixture<SignInComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PropertyComponent ]
+      declarations: [ SignInComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PropertyComponent);
+    fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
