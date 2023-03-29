1742c163e3292d9cc21b44ee4e9743c014760599
Thyago Lucas <thyago.devin@gmail.com>
2023-03-29 00:28:49 -0400

    feat: token interceptor implementeds

--- frontend/projex/src/app/app.module.ts
+++ frontend/projex/src/app/app.module.ts
@@ -1,9 +1,11 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './features/auth/components/sign-in/sign-in.component';
+import { TokenInterceptor } from './features/auth/interceptors/token.interceptor';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,13 @@ import { SignInComponent } from './features/auth/components/sign-in/sign-in.comp
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+			provide:HTTP_INTERCEPTORS,
+			useClass: TokenInterceptor,
+			multi: true
+		},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
